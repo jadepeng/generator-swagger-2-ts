b674cc0a07985d16a0f253326156cce478fb981c
jadepeng <jqpeng@iflytek.com>
2020-05-22 18:05:12 +0800

    支持传参数

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,25 +17,39 @@ module.exports = class extends Generator {
       )
     );
 
-    const prompts = [
-      {
+    var props = {};
+    var prompts = [];
+
+    if (this.options.swaggerUrl) {
+      props.swaggerUrl = this.options.swaggerUrl;
+    } else {
+      prompts.push({
         type: 'input',
         name: 'swaggerUrl',
         message: 'please input swagger url,eg:http://localhost:8080/swagger-ui.html',
         default: 'http://localhost:8080/swagger-ui.html'
-      },
-      {
+      });
+    }
+
+    if (this.options.className) {
+      props.className = this.options.className;
+    } else {
+      prompts.push({
         type: 'input',
         name: 'className',
         message: 'please enter class Name of the generated API',
         default: 'API'
-      },
-      {
+      });
+    }
+
+    if (this.options.type) {
+      props.type = this.options.type;
+    } else {
+      prompts.push({
         type: 'list',
         name: 'type',
         message: 'please choose generated API type , js or typescript',
-        choices: [
-          {
+        choices: [{
             name: 'typescript',
             value: 'typescript'
           },
@@ -44,30 +58,42 @@ module.exports = class extends Generator {
             value: 'js'
           }
         ]
-      },
-      {
+      });
+    }
+
+    if (this.options.outputFile) {
+      props.outputFile = this.options.outputFile;
+    } else {
+      prompts.push({
         type: 'input',
         name: 'outPutFile',
         message: 'Please enter the API file name',
         default: 'api.ts'
-      }
-    ];
+      });
+    }
 
-    return this.prompt(prompts).then(props => {
-      this.props = props;
-    });
+    if (prompts.length) {
+      return this.prompt(prompts).then(_props => {
+        this.props = Object.assign(props, _props);
+      });
+    }
+    this.props = props;
+    return this;
   }
 
   writing() {
+    console.log(this.props);
     var swaggerUrl = this.props.swaggerUrl.replace('swagger-ui.html', 'v2/api-docs');
     var clsName = this.props.className;
-    var outPutFile = this.props.outPutFile;
+    var outPutFile = this.props['outputFile'];
     var isTypescript = this.props.type === "typescript";
     axios.get(swaggerUrl).then(response => {
       if (response.status == 200) {
         var swagger = response.data;
-        var swaggerData = codegen.getViewForSwagger(
-          { swagger: swagger, className: clsName },
+        var swaggerData = codegen.getViewForSwagger({
+            swagger: swagger,
+            className: clsName
+          },
           'typescript'
         );
         swaggerData.defaultDomain = this.props.swaggerUrl.replace('/swagger-ui.html', '');
